bb6169b9d922fce7bbf70378f8047e85b9170bf9
이충희 <dreamjr@gmail.com>
2020-10-04 08:34:39 +0900

    logback/spring profile 설정

--- src/main/java/com/dreamjr/baseapi/controller/TestController.java
+++ src/main/java/com/dreamjr/baseapi/controller/TestController.java
@@ -3,9 +3,11 @@ package com.dreamjr.baseapi.controller;
 import com.dreamjr.baseapi.domain.TestModel;
 import com.dreamjr.baseapi.service.ITestService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
@@ -14,6 +16,9 @@ public class TestController {
 
     @GetMapping("/test1")
     public TestModel test1() {
+        log.info("!!! getTest1 controller info !!!");
+        log.debug("!!! getTest1 controller debug !!!");
+        log.error("!!! getTest1 controller error !!!");
         return testService.getTest1();
     }
 
--- src/main/java/com/dreamjr/baseapi/service/TestService.java
+++ src/main/java/com/dreamjr/baseapi/service/TestService.java
@@ -1,18 +1,25 @@
 package com.dreamjr.baseapi.service;
 
 import com.dreamjr.baseapi.domain.TestModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class TestService implements ITestService{
 
     @Override
     public TestModel getTest1() {
+        log.info("### getTest1 service info ###");
+        log.debug("### getTest1 service debug ###");
+        log.error("### getTest1 service error");
         return new TestModel(1, "content-1");
     }
 
     @Override
     public TestModel getTest2() {
+        log.info("### getTest2 info ###");
+        log.debug("### getTest2 debug ###");
         return new TestModel(2, "content-2");
     }
 }
